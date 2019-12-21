8bc10031ed16da1f67e7aa0337a1a11da913328f
belindabee <belindabee42@gmail.com>
2019-12-21 11:37:50 +0700

    navbar design

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,13 +5,26 @@ import kayumanis from '../component/asset/kayumanis.png'
 class Navbar extends Component {
     render() {
         return (
-         <nav className="navbar navbar-expand-sm navbar-dark px-sm">
+         <nav className="navbar navbar-expand-sm  bg-dark navbar-dark px-sm">
              {/* 
              https://www/iconfinder.com/icons/1243689/call_phone_icon Creative Commons (Attribution 3.0 Unported);
              https://www/iconfinder.com/Makoto_msk
              */}
              <Link to='/'>
-                <img src={kayumanis} alt='store' className='navbar-brand' width='60'/>
+                <img src={kayumanis} alt='store' className='navbar-brand' width='30'/>
+             </Link>
+             <ul className="navbar-nav align-items-center">
+             <li classname="nav-item ml-5">
+                 <Link to="/" className="nav-link">
+                     products
+                 </Link>
+             </li>
+             </ul>
+             <Link to="/cart" clasName="ml-auto">
+                 <button>
+                 <i class="fas fa-shopping-cart"/>
+                     my cart
+                 </button>
              </Link>
          </nav>
         )
